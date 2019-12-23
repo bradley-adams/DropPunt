1fdd9b1a933df1907665e41dcdcb4bfddd7f56dd
Bradley Adams <bradley.wayne.adams@gmail.com>
2019-12-24 10:24:07 +1300

    routes and scripts

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 let express = require('express')
 let server = express()
+let hbs = require('express-handlebars')
+let routes = require('./routes')
+
+server.engine('hbs', hbs({extname: 'hbs'}))
+server.set('view engine', 'hbs')
+server.use(express.urlencoded({extended: true}))
+
+server.use('/', routes)
 
 module.exports = server
