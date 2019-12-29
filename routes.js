6f4d95574815f717de5260e149eb64ab2a32a2a7
Bradley Adams <bradley.wayne.adams@gmail.com>
2019-12-30 11:36:27 +1300

    removing the need for layout dir

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ router.get('/', (req,res)=>{
 
 router.get('/users', (req,res)=>{
   dbFunctions.getUsers().then((users)=>{
-    res.render('users', {users:users})
+    res.render('users', {layout: false, users:users})
   })
 })
 
